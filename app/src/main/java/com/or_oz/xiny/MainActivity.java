09270cee570966f92a12cdc31f96c74d45265ac4
micohen95 <micohen95@gmail.com>
2016-01-23 15:32:15 -0500

    My Location Works
    MyLocation now works and displays pin on map

--- app/src/main/java/com/or_oz/xiny/MainActivity.java
+++ app/src/main/java/com/or_oz/xiny/MainActivity.java
@@ -1,21 +1,32 @@
 package com.or_oz.xiny;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     MapFragment mMapFragment;
+    private GoogleMap mMap;
+
+    public static final int X_IN_Y_LOCATION_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +35,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         mMapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            Log.d("MainActivity_Map", "Permission already granted");
+            mMapFragment.getMap().setMyLocationEnabled(true);
+        } else {
+            Log.d("MainActivity_Map", "Permission not yet granted");
+
+            //Request Permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    X_IN_Y_LOCATION_PERMISSION);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,12 +60,29 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         .setAction("Action", null).show();
             }
         });
+
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0))
-        .title("we made it"));
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == X_IN_Y_LOCATION_PERMISSION) {
+            if (permissions.length == 1 &&
+                    permissions[0] == android.Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMapFragment.getMap().setMyLocationEnabled(true);
+            } else {
+                Log.d("RequestPermissions", "Permission request denied");
+            }
+        }
     }
 }
