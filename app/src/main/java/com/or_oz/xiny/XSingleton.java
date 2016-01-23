b73874fe128d0b38cc4c586ed7c0ea520a0411cd
micohen95 <micohen95@gmail.com>
2016-01-23 18:21:44 -0500

    updated xsingleton location

--- app/src/main/java/com/or_oz/xiny/XSingleton.java
+++ app/src/main/java/com/or_oz/xiny/XSingleton.java
@@ -1,25 +1,32 @@
 package com.or_oz.xiny;
 
 import android.location.Location;
+import android.support.v4.util.SimpleArrayMap;
 
 /**
  * Created by Michael on 1/23/2016.
  */
 public class XSingleton {
     private static XSingleton ourInstance = new XSingleton();
+    private SimpleArrayMap<Location, XItem> map;
 
     public static XSingleton getInstance() {
         return ourInstance;
     }
 
     private XSingleton() {
+        map = new SimpleArrayMap<Location, XItem>();
     }
 
-    public void addItem(String name, String comments, Location l) {
+    public void addItem(String name, String comments, double x, double y) {
 
     }
 
     private class XItem {
+        String name;
+        String comments;
+
+        double latitude, longitude;
 
     }
 }
