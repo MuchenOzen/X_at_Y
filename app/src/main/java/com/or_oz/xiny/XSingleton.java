b0c261e1d4907ee5055e8b202fde77c3b1b10b55
micohen95 <micohen95@gmail.com>
2016-01-23 18:29:03 -0500

    description of XItem

--- app/src/main/java/com/or_oz/xiny/XSingleton.java
+++ app/src/main/java/com/or_oz/xiny/XSingleton.java
@@ -3,6 +3,9 @@ package com.or_oz.xiny;
 import android.location.Location;
 import android.support.v4.util.SimpleArrayMap;
 
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
 /**
  * Created by Michael on 1/23/2016.
  */
@@ -25,8 +28,27 @@ public class XSingleton {
     private class XItem {
         String name;
         String comments;
-        //BitmapDescriptor icon;
+        BitmapDescriptor icon;
         double latitude, longitude;
+
+        public XItem(String name, String comments, double lat, double longi){
+            this.name = name;
+            this.comments = comments;
+            this.latitude = lat;
+            this.longitude = longi;
+
+            switch(name){
+                case "Police":
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN);
+                    break;
+                case "Food":
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE);
+                    break;
+                default:
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
+                    break;
+            }
+        }
 
     }
 }
